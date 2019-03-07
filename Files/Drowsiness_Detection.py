bfd5ee244d50be89ddcb29702d068a17798bfa14
Lea Marolt Sonnenschein <leyamars@gmail.com>
2019-03-07 23:07:02 +0000

    Final code demo upload

--- Files/Drowsiness_Detection.py
+++ Files/Drowsiness_Detection.py
@@ -6,6 +6,7 @@ import imutils
 import dlib
 import cv2
 import playsound
+import pygame
 
 def eye_aspect_ratio(eye):
 	A = distance.euclidean(eye[1], eye[5])
@@ -14,33 +15,92 @@ def eye_aspect_ratio(eye):
 	ear = (A + B) / (2.0 * C)
 	return ear
 
-def sound_alarm(path):
-	playsound.playsound(path, False)
+def sound_alarm(on):
+	if on:
+		pygame.mixer.music.play()
+	else:
+		pygame.mixer.music.stop()
 	
-thresh = 0.40
-frame_check = 20
+# Initialize pygame music player 
+soundFilePath = "Gentle-wake-alarm-clock.mp3"
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(soundFilePath)
+
+thresh = 0.35
+frame_check = 40
 detect = dlib.get_frontal_face_detector()
 predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+
 cap=cv2.VideoCapture(0)
 flag=0
-
-soundFilePath = "lullaby.wav"
 alarm_on= False
+screen_width = 800
 
-img_path = 'overlay.jpg'
-img = cv2.imread(img_path, -1)
+font = cv2.FONT_HERSHEY_DUPLEX
+text = "How about a movie?"
+text_size = cv2.getTextSize(text, font, 1, 2)
+text_width = text_size[0][0]
+text_height = text_size[0][1]
 
-screenWidth = 800
-screenHeightMult = 3/5
+catch_me_path = "catchMe.jpg"
+catch_img = cv2.imread(catch_me_path, -1)
 
-overlayOn = False
+wizard_path = "wizard.jpg"
+wizard_img = cv2.imread(wizard_path, -1)
+
+forest_path = "forrest.jpg"
+forest_img = cv2.imread(forest_path, -1)
+
+show_screen_message = False
+
+# Frame shape is basically the size of the frame, so that we can make our overlay the same size, and
+# center the text in the middle.
+
+def showBlueOverlay(frame_shape):
+	cv2.rectangle(overlay,(0, 0),(frame_shape[1], frame_shape[0]),(203, 139, 48),-1)
+	opacity = 0.3
+	cv2.addWeighted(overlay, opacity, frame, 1 - opacity, 0, frame)
+
+def showOnScreenMessage(frame_shape, should_show):
+	if should_show:
+		middle_x = frame_shape[1]/2
+		middle_y = frame_shape[0]/2
+
+		padding = 20
+
+		text_x = int(middle_x - text_width/2)
+		text_y = int(middle_y - text_height/2 - padding)
+		cv2.putText(frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2)
+
+		catch_img_x = int(middle_x - catch_img.shape[1]/2)
+		catch_img_y = int(middle_y - catch_img.shape[0]/2 + text_height + padding * 2)
+
+		frame[catch_img_y: catch_img_y + catch_img.shape[0], catch_img_x: catch_img_x + catch_img.shape[1]] = catch_img
+
+		forest_img_x = int(catch_img_x - catch_img.shape[1] - padding)
+		forest_img_y = catch_img_y
+
+		frame[forest_img_y: forest_img_y + forest_img.shape[0], forest_img_x: forest_img_x + forest_img.shape[1]] = forest_img
+
+		wizard_img_x = int(catch_img_x + catch_img.shape[1] + padding)
+		wizard_img_y = catch_img_y
+
+		frame[wizard_img_y: wizard_img_y + wizard_img.shape[0], wizard_img_x: wizard_img_x + wizard_img.shape[1]] = wizard_img
 
 while True:
 	ret, frame=cap.read()
-	frame = imutils.resize(frame, width=screenWidth)
+	frame = imutils.resize(frame, width=screen_width)
+	frame_shape = frame.shape
+
+	# First create the image with alpha channel
+	#frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+	# Then assign the mask to the last channel of the image
+	#frame[:, :, 3] = 1
+	
 	overlay = frame.copy()
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -56,37 +116,35 @@ while True:
 		ear = (leftEAR + rightEAR) / 2.0
 		leftEyeHull = cv2.convexHull(leftEye)
 		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [leftEyeHull], -1, (203, 139, 48), 2)
+		cv2.drawContours(frame, [rightEyeHull], -1, (203, 139, 48), 2)
 
 		if ear < thresh:
 			flag += 1
-			print (flag)
+			#print (flag)
 			if flag >= frame_check:
+				show_screen_message = False
+				showBlueOverlay(frame_shape)
 
-				# Green filled in rectangle
-				frame_shape = frame.shape
-				print(frame_shape)
-				print(len(frame[:0]))
-				
-				cv2.rectangle(overlay,(0, 0),(800, 450),(0,140,222),-1)
-				opacity = 0.4
-				cv2.addWeighted(overlay, opacity, frame, 1 - opacity, 0, frame)
-				cv2.putText(frame, "****************SWEET DREAMS ;)****************", (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-				cv2.putText(frame, "****************SWEET DREAMS ;)****************", (10,400),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-				
 				if not alarm_on:
 					alarm_on = True
-					sound_alarm(soundFilePath)
+					sound_alarm(alarm_on)
 		else:
 			flag = 0
+			if alarm_on:
+				show_screen_message = True
 			alarm_on = False
+			sound_alarm(alarm_on)
+
+		showOnScreenMessage(frame_shape, show_screen_message)
 
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord("q"):
 		break
+	if key == ord("r"):
+		# Reset to nothing showing
+		show_screen_message = False
+
 cv2.destroyAllWindows()
 cap.stop()
